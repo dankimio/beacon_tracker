5c8d8ce3a9b4c10c661d9a9db525e906cbe8a059
Dan Kim <itsdanya@gmail.com>
2017-05-21 17:11:44 +0300

    Location: reverse geocode address

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,4 +4,16 @@ class Location < ApplicationRecord
 
   validates :latitude, inclusion: { in: -90..90 }
   validates :longitude, inclusion: { in: -180..180 }
+
+  after_create :reverse_geocode
+
+  reverse_geocoded_by :latitude, :longitude do |beacon, results|
+    geo = results.first
+    return unless geo
+
+    beacon.address = geo.address
+    beacon.city = geo.city
+    beacon.country = geo.country
+    beacon.country_code = geo.country_code
+  end
 end
