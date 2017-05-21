1e5ff803560157d2bfb2e1ed3ed8d836897ee476
Dan Kim <itsdanya@gmail.com>
2017-05-21 18:36:40 +0300

    API: locations

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
     resources :beacons, only: [:index, :show, :update], param: :major_minor do
       resources :activations, only: [:create]
+      resources :locations, only: [:index, :create]
     end
   end
 
---
+++ test/controllers/api/locations_controller_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class API::LocationsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @location = locations(:location)
+    @beacon = @location.beacon
+  end
+
+  test 'should get index' do
+    get api_beacon_locations_url(@beacon.major_minor, api_token: api_token), as: :json
+    assert_response :success
+  end
+
+  test 'should create location' do
+    assert_difference 'Location.count' do
+      post api_beacon_locations_url(@beacon.major_minor, api_token: api_token), params: {
+        location: @location.attributes
+      }, as: :json
+    end
+
+    assert_response 201
+  end
+end
