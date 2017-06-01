da16a12f498aef0b5ae7a1441b626371ecd6ea72
Dan Kim <itsdanya@gmail.com>
2017-06-02 02:58:05 +0300

    Admin: edit and destroy beacons

--- app/controllers/admin/beacons_controller.rb
+++ app/controllers/admin/beacons_controller.rb
@@ -1,5 +1,6 @@
 class Admin::BeaconsController < Admin::AdminController
   before_action :authenticate_admin!
+  before_action :set_beacon, only: [:edit, :update, :destroy]
 
   def index
     @beacons = Beacon.all
@@ -10,18 +11,42 @@ class Admin::BeaconsController < Admin::AdminController
   end
 
   def create
-    @beacon = Beacon.new(beacon_params)
+    @beacon = Beacon.new(new_beacon_params)
 
     if @beacon.save
-      redirect_to admin_beacons_url, notice: 'Beacon was successfully created'
+      redirect_to admin_beacons_url, notice: t('.notice')
     else
       render :new
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @beacon.update(edit_beacon_params)
+      redirect_to admin_beacons_url, notice: t('.notice')
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @beacon.destroy
+    redirect_to admin_beacons_url, notice: t('.notice')
+  end
+
   private
 
-  def beacon_params
+  def set_beacon
+    @beacon = Beacon.find(params[:id])
+  end
+
+  def new_beacon_params
     params.require(:beacon).permit(:major, :minor, :passcode)
+  end
+
+  def edit_beacon_params
+    params.require(:beacon).permit(:passcode)
   end
 end
